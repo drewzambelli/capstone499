c58295e1e911f9bff9f1905f2a44e84161e4f6b1
Dariel <dariel.martinez84@myhunter.cuny.edu>
2024-06-13 16:51:18 -0400

    add mainlocal

---
+++ src/components/MainLocal.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { APIProvider, Map } from '@vis.gl/react-google-maps'
+import env from '../env/env'
+import NavbarLocal from './NavbarLocal'
+
+export interface MainProps{
+  children: React.ReactNode
+}
+
+
+function MainLocal(props: MainProps) {
+  return (
+    <APIProvider apiKey={env.GOOGLE_MAPS_API_KEY}>
+      <NavbarLocal/>
+      <Map
+        style={{width:'100vw', height: '100vh'}}
+        defaultCenter={{lat:  40.730610, lng: -73.935242}}
+        defaultZoom={12}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}/>
+      {props.children}
+    </APIProvider>
+  )
+}
+
+export default MainLocal
