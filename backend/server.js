77b7a63b8edc960b4df532d2cabf1088e5a57c5f
Dariel Martinez <dariel.martinez84@myhunter.cuny.edu>
2024-07-01 20:36:21 -0400

    handle submission done with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose')
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
 const port = 3000
 
@@ -11,20 +12,58 @@ const pass = process.env.MONGO_PASSWORD;
 const username = encodeURIComponent(user);
 const password = encodeURIComponent(pass);
 
+app.use(cors())
+app.use(express.json())
+
+const DataSchema = new mongoose.Schema({
+    id: Number,
+    username: String,
+    firstname: String,
+    lastname: String,
+    age: Number
+})
+
+const Data = mongoose.model('Data', DataSchema);
+
+app.post('/api/postData', async(req,res)=>{
+    try{
+        const db =  await testConnection();
+        const body =await req.body
+        if(body){
+            console.log(body)
+            const inputData = new Data({
+                id:await db.countDocuments() + 1,
+                username: body.userName,
+                firstname: body.firstName,
+                lastname: body.lastName,
+                age: body.age
+            })
+            const result = await db.insertOne(inputData)
+            res.status(200).send(`Data inserted with ID ${result.insertedId}`);
+            console.log(`Data inserted with ID ${result.insertedId}`);
+        }
+    }catch(error){
+        console.error(error);
+        res.status(500).send("ERROR LOADING DATA")
+    }
+})
+
 async function testConnection(){
     const uri = `mongodb+srv://${username}:${password}@locally-cluster-1.crkbqzb.mongodb.net/?retryWrites=true&w=majority&appName=locally-cluster-1`;
-    console.log(uri)
     const client = new MongoClient(uri);
     let conn;
     try {
         conn = await client.connect();
-        const database = client.db('capstone')
-        const locally = database.collection('locally')
+        gooseConn = await mongoose.connect(uri);
+        const database = client.db('capstone');
+        const locally = database.collection('locally-usernames');
         return locally;
     } catch (e) {
         console.error(e);
     }
 }
+
+
 
 app.get('/api/checkUserExists/:username', async (req,res)=>{
     try{
@@ -33,7 +72,10 @@ app.get('/api/checkUserExists/:username', async (req,res)=>{
         const query = {username: checkUsername} 
         const result = await db.findOne(query);
         if(!result){console.log("NOT THERE")}
-        else console.log(result)
+        else {
+            console.log(result)
+            res.status(200).send(result)
+        }
     }catch(e){
         console.log(e)
     }
--- src/components/userName.tsx
+++ src/components/userName.tsx
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
-
-interface UserNameProps {
-  onUserDataSubmit: (userData: UserData) => void;
-}
-
-interface UserData {
-  username: string;
-  firstName: string;
-  lastName: string;
-  age: number;
-}
-
-const UserName: React.FC<UserNameProps> = ({ onUserDataSubmit }) => {
+function UserName(){
+  interface UserData {
+    userName: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+  }
   const [userData, setUserData] = useState<UserData>({
-    username: '',
+    userName: '',
     firstName: '',
     lastName: '',
     age: 0,
-  });
+  })
 
-  const [submitted, setSubmitted] = useState(false);
+  const [submitted, setSubmitted] = useState<Boolean>(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setUserData({ ...userData, [name]: value });
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); //need this here. If not, the whole form just reloads like it is a fresh page
-    onUserDataSubmit(userData);
+    console.log(userData)
+    await fetch('http://localhost:3000/api/postData', {
+      method: 'POST',
+      headers:{'Content-Type': 'application/json'},
+      body: JSON.stringify(userData)
+    })
+
     setSubmitted(true);
+
   };
 
   if (submitted) {
@@ -47,8 +48,8 @@ const UserName: React.FC<UserNameProps> = ({ onUserDataSubmit }) => {
         <input
           type="text"
           placeholder="User name - people see this"
-          name="username"
-          value={userData.username}
+          name="userName"
+          value={userData.userName}
           onChange={handleInputChange}
           required
         />
